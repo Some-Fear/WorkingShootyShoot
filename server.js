3ffbec935c6613cdad9b72e6cafe995b1c053019
Atlas1998 <32174176+Atlas1998@users.noreply.github.com>
2020-11-24 22:29:09 +0000

    Update server.js

--- server.js
+++ server.js
@@ -43,3 +43,29 @@ io.on('connection', function(socket) {
 setInterval(function() {
   io.sockets.emit('state', players);
 }, 1000 / 60);
+
+
+
+// Database sign code that should validate username and password 
+
+//var isValidPassword = function(data,cb){
+	//db.account.find({username:data.username,password:data.password},function(err,res){
+		//if(res.length > 0)
+			//cb(true);
+		//else
+			//cb(false);
+	//});
+//}
+//var isUsernameTaken = function(data,cb){
+	//db.account.find({username:data.username},function(err,res){
+		//if(res.length > 0)
+			//cb(true);
+		//else
+			//cb(false);
+	//});
+//}
+//var addUser = function(data,cb){
+	//db.account.insert({username:data.username,password:data.password},function(err){
+		//cb();
+	//});
+//}
